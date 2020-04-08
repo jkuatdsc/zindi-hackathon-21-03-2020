bdeb4f9be3aba1c2d8998c4d975bc52653364463
0x6f736f646f <blackd0t@protonmail.com>
2020-04-08 22:22:30 +0300

    Adding data augmentation while copying files **It takes a lot of time

--- UmojaHack#1/Src/Scripts/prepare_data.py
+++ UmojaHack#1/Src/Scripts/prepare_data.py
@@ -1,17 +1,40 @@
+import numpy as np
 import os
+
+from keras.preprocessing.image import ImageDataGenerator, img_to_array, load_img
+from tqdm import tqdm
 
 image_dir = "../../Data/Raw/UmojaHack#1:SAEON_Identifying_marine_invertebrates"
 train_dir = image_dir + "/train_small"
 test_dir = image_dir + "/test_small"
 
 
+def augment_images(old_path, new_path):
+    image = np.expand_dims(img_to_array(load_img(path=old_path)), axis=0)
+    aug = ImageDataGenerator(rescale=1. / 255,
+                             rotation_range=40,
+                             width_shift_range=0.2,
+                             height_shift_range=0.2,
+                             shear_range=0.2,
+                             zoom_range=0.2,
+                             horizontal_flip=True,
+                             fill_mode='nearest'
+                             )
+    total = 0
+    image_gen = aug.flow(image, batch_size=1, save_prefix="gen", save_to_dir=new_path, save_format="jpeg")
+    for _ in image_gen:
+        total += 1
+        if total == 100:
+            break
+
+
 def make_directories():
     print("Making directories")
-    if not os.path.exists('../../Data/Processed/Train')
+    if not os.path.exists('../../Data/Processed/Train'):
         os.makedirs('../../Data/Processed/Train')
-    if not os.path.exists('../../Data/Processed/Val')
+    if not os.path.exists('../../Data/Processed/Val'):
         os.makedirs('../../Data/Processed/Val')
-    if not os.path.exists('../../Data/Processed/Test')
+    if not os.path.exists('../../Data/Processed/Test'):
         os.makedirs('../../Data/Processed/Test')
     for root, dirs, files in os.walk(train_dir):
         for individual_dir in dirs:
@@ -36,19 +59,21 @@ def modify_directories():
 
 def copy_data():
     print("Copying images to processed directory")
-    for root, dirs, files in os.walk(train_dir):
+    for root, dirs, files in tqdm(os.walk(train_dir)):
         total_files_number = len(files)
         i = 0
-        for file in files:
+        for file in tqdm(files):
             if file.endswith('jpeg') or file.endswith('JPEG'):
                 if (i % 3) == 0:
                     new_path = '../../Data/Processed/Val/' + str(root.split("/")[-1])
                     command = "cp {} {}".format(os.path.join(root, file), new_path)
                     os.system(command)
+
                 else:
                     new_path = '../../Data/Processed/Train/' + str(root.split("/")[-1])
                     command = "cp {} {}".format(os.path.join(root, file), new_path)
                     os.system(command)
+                    augment_images(os.path.join(root, file), new_path)
                 i += 1
 
     for root, dirs, files in os.walk(test_dir):
