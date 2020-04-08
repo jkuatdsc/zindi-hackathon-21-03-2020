434e7ee6d851bbf63d78bfe45cc23f423b496925
0x6f736f646f <blackd0t@protonmail.com>
2020-04-08 23:53:39 +0300

    Adding loading data to modelling

--- UmojaHack#1/Src/Scripts/modelling.py
+++ UmojaHack#1/Src/Scripts/modelling.py
@@ -1,97 +1,99 @@
+import cv2
+import os
+from tqdm import tqdm
+import time
+
+import matplotlib.pyplot as plt
 import numpy as np
-from keras.preprocessing.image import ImageDataGenerator
-from keras.models import Sequential
-from keras.layers import (Activation, AveragePooling2D, Flatten, InputLayer,
-                          BatchNormalization, Convolution2D, Dense, MaxPooling2D,
-                          Dropout, GlobalAveragePooling2D)
-from keras.utils.np_utils import to_categorical
+from imutils import paths
 from keras.applications import InceptionV3
 from keras.callbacks import TensorBoard, ModelCheckpoint
-import time
-import matplotlib.pyplot as plt
+from keras.layers import (Dense, GlobalAveragePooling1D)
+from keras.models import Sequential
+from keras.preprocessing.image import img_to_array
+from keras.utils.np_utils import to_categorical
+from sklearn.model_selection import train_test_split
 
 top_model_model_path = '../../Output/Models/model.h5'
 top_model_weights_path = '../../Output/Models/weights.h5'
 train_data_path = '../../Data/Processed/Train'
 validation_data_path = '../../Data/Processed/Val'
-img_width, img_height = 512, 384
-epochs = 100
+img_width, img_height = 150, 150
+epochs = 10
 batch_size = 8
 
 
-def train():
-    datagen = ImageDataGenerator(rescale=1. / 255)
-    generator = datagen.flow_from_directory(
-        train_data_path,
-        target_size=(img_width, img_height),
-        batch_size=batch_size,
-        class_mode='categorical',
-        shuffle=False)
+def load_data(dir_path, split=True):
+    """
 
-    # nb_train_samples = len(generator.filenames)
-    num_classes = len(generator.class_indices)
+    :type dir_path: str
+    :param dir_path: directory path
+    :type split: bool
+    """
+    # initialize the data and labels
+    data = []
+    labels = []
+    dir_labels = ()
+    num_class = 0
 
-    # load the bottleneck features saved earlier
-    train_data = np.load('../../Output/Models/bottleneck_features_train.npy')
+    # finding the labels
+    print("[INFO] Finding Labels...")
+    for file in tqdm(os.listdir(dir_path)):
+        temp_tuple = (file, 'null')
+        dir_labels = dir_labels + temp_tuple
+        dir_labels = dir_labels[:-1]
+        num_class = num_class + 1
 
-    # get the class labels for the training data, in the original order
-    train_labels = generator.classes
+    imagepaths = sorted(list(paths.list_images(dir_path)))
 
-    # convert the training labels to categorical vectors
-    train_labels = to_categorical(train_labels, num_classes=num_classes)
+    # loop over the input images
+    print("[INFO] Converting images to array...")
+    for imagePath in tqdm(imagepaths):
+        image = cv2.imread(imagePath)
+        # image = cv2.resize(image, (img_height, img_width))
+        image = img_to_array(image)
+        data.append(image)
 
-    generator = datagen.flow_from_directory(
-        validation_data_path,
-        target_size=(img_width, img_height),
-        batch_size=batch_size,
-        class_mode=None,
-        shuffle=False)
+        label = imagePath.split(os.path.sep)[-2]
 
-    validation_data = np.load('../../Output/Models/bottleneck_features_validation.npy')
+        for i in range(num_class):
+            if label == dir_labels[i]:
+                label = i
+        labels.append(label)
 
-    validation_labels = generator.classes
-    validation_labels = to_categorical(validation_labels, num_classes=num_classes)
+    # scale the raw pixel intensities to the range [0, 1]
+    data = np.array(data, dtype="float") / 255.0
+    labels = np.array(labels)
+    if split:
+        (trainX, testX, trainY, testY) = train_test_split(data, labels, test_size=0.33, random_state=42)
 
-    # model = applications.vgg16.VGG16(include_top=False, weights="imagenet", input_shape=(224, 224, 3))
-    # new_input = model.input
-    # hidden_layer = model.layers[-1].output
-    # model = Model(new)
-    # image_features_extract_model = tf.keras.Model(new_input, hidden_layer)
+        # convert the labels from integers to vectors
+        trainY = to_categorical(trainY, num_classes=num_class)
+        testY = to_categorical(testY, num_classes=num_class)
 
-    pool_size = (2, 2)
+        return trainX, trainY, testX, testY
+    else:
+        return data, labels
 
-    hidden_num_units = 50
-    output_num_units = 10
-    input_num_units = 784
-    hidden1_num_units = 500
-    hidden2_num_units = 500
-    hidden3_num_units = 500
-    hidden4_num_units = 500
-    hidden5_num_units = 500
-    output_num_units = 10
-    print(train_data.shape)
+
+def train(xtrain: list, ytrain: list, xtest: list, ytest: list, xval: list, yval: list):
+
+    base_model = InceptionV3(
+        input_shape=(img_height, img_width, 3),
+        include_top=False,
+        weights="imagenet"
+    )
+    base_model.trainable = False
+
+    global_average_layer = GlobalAveragePooling1D()
+    prediction_layer = Dense(137)
     model = Sequential([
-        Dense(output_dim=hidden1_num_units, input_dim=train_data.shape[1:], activation='relu'),
-        Dropout(0.2),
-        Dense(output_dim=hidden2_num_units, input_dim=hidden1_num_units, activation='relu'),
-        Dropout(0.2),
-        Dense(output_dim=hidden3_num_units, input_dim=hidden2_num_units, activation='relu'),
-        Dropout(0.2),
-        Dense(output_dim=hidden4_num_units, input_dim=hidden3_num_units, activation='relu'),
-        Dropout(0.2),
-        Dense(output_dim=hidden5_num_units, input_dim=hidden4_num_units, activation='relu'),
-        Dropout(0.2),
-
-        Dense(output_dim=output_num_units, input_dim=hidden5_num_units, activation='softmax'),
+        base_model,
+        global_average_layer,
+        prediction_layer
     ])
 
-    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-    # model.add(Convolution2D(filters=num_classes, kernel_size=(3, 3), padding='same'))
-    # model.add(GlobalAveragePooling2D())
-    # model.add(Activation('softmax', name='predictions'))
-
-    # model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
     tensorboard = TensorBoard(log_dir="logs/{}".format(time.time()))
     filepath = "model-{epoch:02d}.h5"
@@ -100,17 +102,17 @@ def train():
     # train the network
     print("[INFO] training network...")
 
-    history = model.fit(train_data, train_labels,
+    history = model.fit(xtrain, ytrain,
                         epochs=epochs,
                         batch_size=batch_size,
                         verbose=1,
                         callbacks=[tensorboard, checkpoint],
-                        validation_data=(validation_data, validation_labels))
+                        validation_data=(xtest, ytest))
 
     model.save_weights(top_model_weights_path)
     model.save(top_model_model_path)
 
-    (eval_loss, eval_accuracy) = model.evaluate(validation_data, validation_labels, batch_size=batch_size, verbose=1)
+    (eval_loss, eval_accuracy) = model.evaluate(xval, yval, batch_size=batch_size, verbose=1)
 
     print("[INFO] accuracy: {:.2f}%".format(eval_accuracy * 100))
     print("[INFO] Loss: {}".format(eval_loss))
@@ -143,5 +145,7 @@ def make_plot(history):
 
 
 if __name__ == "__main__":
-    history = train()
+    xtrain, ytrain, xtest, ytest = load_data(train_data_path, split=True)
+    xval, yval = load_data(validation_data_path, split=False)
+    history = train(xtrain, ytrain, xtest, ytest, xval, yval)
     make_plot(history)
